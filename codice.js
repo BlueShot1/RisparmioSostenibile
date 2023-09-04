71c1981f7e706c4f6f340bcb2fd600932df51dea
BlueShot1 <73930392+BlueShot1@users.noreply.github.com>
2023-09-04 15:51:51 +0200

    olti ptrutana

--- codice.js
+++ codice.js
@@ -1,12 +1,28 @@
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
-
-const prezzoBenzina = 2;
+const prezzoBenzina = 1.873;
 function drawChart() {
+
+
+
     var km = document.getElementById("km").value;
     var kmXl = document.getElementById("kmXl").value;
     var prezzoIniziale = document.getElementById("initPrice").value;
     var anni = document.getElementById("year").value;
+
+
+    var km1 = document.getElementById("km1").value;
+    var kmXl1 = document.getElementById("kmXl1").value;
+    var prezzoIniziale1 = document.getElementById("initPrice1").value;
+
+    var data = google.visualization.arrayToDataTable([
+      ['Year', 'Sales', 'Expenses'],
+      ['2004',  1000,      400],
+      ['2005',  1170,      460],
+      ['2006',  660,       1120],
+      ['2007',  1030,      540]
+    ]);
+
   /*var data = google.visualization.arrayToDataTable([
     ['Year', 'prova', 'Expenses'],
     ['2004',  1000,      400],
@@ -17,9 +33,10 @@ function drawChart() {
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'anni');
     data.addColumn('number','spesa');
+    data.addColumn('number','spesa1');
     data.addRows([
-        ['0',Number(prezzoIniziale)],
-        [anni,Number(prezzoIniziale)+ Number((Number(km) * Number(anni))/Number(kmXl)*prezzoBenzina)]
+        ['0',Number(prezzoIniziale),Number(prezzoIniziale1)],
+        [anni,Number(prezzoIniziale)+ Number((Number(km) * Number(anni))/Number(kmXl)*prezzoBenzina),Number(prezzoIniziale1)+ Number((Number(km1) * Number(anni))/Number(kmXl1)*prezzoBenzina)]
     ]);
 
   var options = {
